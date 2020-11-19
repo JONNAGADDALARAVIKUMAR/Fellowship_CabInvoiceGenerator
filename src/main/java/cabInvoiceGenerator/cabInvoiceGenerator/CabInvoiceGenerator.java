df23c748b081db84eea80b6a20b8b619490661d8
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-19 14:41:18 +0530

    Welcome message

---
+++ src/main/java/cabInvoiceGenerator/cabInvoiceGenerator/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package cabInvoiceGenerator.cabInvoiceGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class CabInvoiceGenerator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
